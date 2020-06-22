8da5a7c5b5043232387720c9e49a1e14f3c7878c
IbrajKhan <ibraj.grd@gmail.com>
2020-06-23 04:10:54 +0530

    Extract to method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,16 +5,20 @@ class Counter extends Component {
         count: 1
     };
   render() {
-      let classes = "badge m-2 p-2 badge-"
-      classes += (this.state.count === 0) ? "primary" : "warning"
 
     return (
       <div>
-        <span className = {classes}>{this.formatCount()}</span>
+        <span className = {this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className= "btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+    getBadgeClasses() {
+        let classes = "badge m-2 p-2 badge-";
+        classes += (this.state.count === 0) ? "primary" : "warning";
+        return classes;
+    }
+
   formatCount(){
       const {count} = this.state
       return count === 0 ?  "zero" : count
