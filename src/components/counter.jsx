85b85cc470e04f6e9eb63541c2b08f94a0f4e4d4
IbrajKhan <ibraj.grd@gmail.com>
2020-06-26 08:26:47 +0530

    created counters.jsx file
    composing the components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,16 +4,18 @@ class Counter extends Component {
   state = {
     count: 0,
   };
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(this.state.count);
     this.setState({count : this.state.count + 1})
     
   };
+   
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={ () => this.handleIncrement(this.state.count)}
           className="btn btn-secondary btn-sm"
         >
           Increment
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Counter from "./components/counter"
+import Counters from "./components/counters"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>,
   document.getElementById('root')
 );
