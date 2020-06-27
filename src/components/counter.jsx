a4b9f940d49f40ba3c54464b80a123c355e51873
IbrajKhan <ibraj.grd@gmail.com>
2020-06-27 08:09:06 +0530

    Raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,35 +2,35 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value,
   };
-  handleIncrement = product => {
-    console.log(this.state.count);
-    this.setState({count : this.state.count + 1})
-    
+  handleIncrement = (product) => {
+    console.log(this.state.value);
+    this.setState({ value: this.state.value + 1 });
   };
-   
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={ () => this.handleIncrement(this.state.count)}
+          onClick={() => this.handleIncrement(this.state.value)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
+        <button onClick = {() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 p-2 badge-";
-    classes += this.state.count === 0 ? "primary" : "warning";
+    classes += this.state.value === 0 ? "primary" : "warning";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,16 +5,26 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 0, value: 0 },
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
     ],
   };
+
+  handelDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
-        {this.state.counters.map( counter => (
-          <Counter key={counter.id} />
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            counter = {counter}
+            onDelete={this.handelDelete}
+          />
         ))}
       </div>
     );
